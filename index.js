1b08c663ac2489075aae6e2d03e7f37365447312
Nut Chanyong <nutjaa@msn.com>
2017-05-13 10:39:19 +0700

    Remove type ahead

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import(app.bowerDirectory + '/typeahead.js/dist/typeahead.bundle.js');
-
     app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput.js');
     app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput.css');
-    app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput-typeahead.css');
   }
 };
