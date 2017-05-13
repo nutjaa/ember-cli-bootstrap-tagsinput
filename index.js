3d95ecc688d5ecec7c556de18cae42aace463262
Nut Chanyong <nutjaa@msn.com>
2017-05-13 17:40:20 +0700

    Fastboot ++

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ module.exports = {
 
   included: function(app) {
     this._super.included.apply(this, arguments);
-
-    app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+    	app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput.js');
+    }
     app.import(app.bowerDirectory + '/bootstrap-tagsinput/src/bootstrap-tagsinput.css');
   }
 };
